c0bbd529300565c900ff6b48a75679ad842b6261
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2021-11-22 11:03:31 +0530

    Navigation bar issue fixed

--- app/views/request/index.php
+++ app/views/request/index.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
     integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
-    <link rel="stylesheet" href="<?php echo URLROOT;?>./css/home_styles.css" />
-    
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-    
-    <title>HexClan</title>
+  <link rel="stylesheet" href="<?php echo URLROOT;?>./css/home_styles.css" />
+
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <title>HexClan</title>
 </head>
+
 <body>
 
-<nav class="nav navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="nav navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <img src="" alt="" width="30" height="24" class="d-inline-block align-text-top">
@@ -37,40 +39,51 @@
             <a class="nav-link" href=""><i class="fa fa-bell"></i></a>
           </li>
           <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
-        <i class="fa fa-user"></i>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Recent Donates</a>
-          <a class="dropdown-item" href="#">Settings</a>
-          <a><hr class="dropdown-divider"></a>
-          <a class="dropdown-item" href="#">Logout</a>
-        </div>
-      </li>
-          
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+              data-toggle="dropdown" aria-expanded="false">
+              <i class="fa fa-user"></i>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Recent Donates</a>
+              <a class="dropdown-item" href="#">Settings</a>
+              <a>
+                <hr class="dropdown-divider"></a>
+              <a class="dropdown-item" href="<?php echo URLROOT ?>/users/signout">Logout</a>
+            </div>
+          </li>
+
         </ul>
 
       </div>
     </div>
   </nav>
 
-  <section class="colored-section bg-light text-white" id="Home" >
+  <section class="colored-section bg-light text-white" id="Home">
 
     <div class="row text-dark text-center">
       <div class="col-lg-6 ">
         <h2>Requests</h2>
-      
-    </div>
-    <div class="col-lg-6" >
-    <form class="d-flex">
-      
-        <button class="btn btn-primary" type="submit"><i class="fa fa-pencil"></i> Create New</button>
-      </form>
-    </div>
+
+      </div>
+      <div class="col-lg-6">
+        <form class="d-flex">
+
+          <button class="btn btn-primary" type="submit"><i class="fa fa-pencil"></i> Create New</button>
+        </form>
+      </div>
     </div>
   </section>
 
-
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  </script>
 
 </body>
+
 </html>
