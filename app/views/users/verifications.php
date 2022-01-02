894f47c50ae814ed47904c2306181e1624de7401
Yasiru Lakshan <yasiru1999@gmail.com>
2022-01-02 17:34:10 +0530

    User Verifiation Page done

--- app/views/users/verifications.php
+++ app/views/users/verifications.php
@@ -1,34 +1,147 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
-  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
+
   <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="<?php echo URLROOT;?>/css/home_styles.css" />
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/home_styles.css" />
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/user_reg.css" />
 
   <title>HexClan</title>
-  <?php require APPROOT . '/views/inc/favicon.php';?>
+  <?php require APPROOT . '/views/inc/favicon.php'; ?>
 </head>
-<body>
-    <?php require APPROOT . '/views/inc/admin_navbar.php';?>
-    <h1>User Verifications</h1>
 
-    
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+<body>
+  <?php require APPROOT . '/views/inc/admin_navbar.php'; ?>
+  <section class="colored-section bg-light text-dark" id="Home">
+
+    <div class="card card-image">
+      <img class="verify-img" src="<?php echo URLROOT; ?>../images/User_verification.jpg" alt="pic" />
+    </div>
+    <br>
+    <h1>Pending Requests</h1>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card1">
+            <div class="card-in">
+              <div>
+                <h3>Dimuth Karunarathna</h3>
+                <br>
+                <h5>No.118, Nilawadana Road, Homagama, </h5>
+                <h5>Colombo 08</h5>
+                <h5>071 92 54 679</h5>
+              </div>
+              <br>
+              <div class="row">
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-danger btn-block">Decline</button>
+                </div>
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-success btn-block">Verify</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card1">
+            <div class="card-in">
+              <div>
+                <h3>Dimuth Karunarathna</h3>
+                <br>
+                <h5>No.118, Nilawadana Road, Homagama, </h5>
+                <h5>Colombo 08</h5>
+                <h5>071 92 54 679</h5>
+              </div>
+              <br>
+              <div class="row">
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-danger btn-block">Decline</button>
+                </div>
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-success btn-block">Verify</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card1">
+            <div class="card-in">
+              <div>
+                <h3>Dimuth Karunarathna</h3>
+                <br>
+                <h5>No.118, Nilawadana Road, Homagama, </h5>
+                <h5>Colombo 08</h5>
+                <h5>071 92 54 679</h5>
+              </div>
+              <br>
+              <div class="row">
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-danger btn-block">Decline</button>
+                </div>
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-success btn-block">Verify</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card1">
+            <div class="card-in">
+              <div>
+                <h3>Dimuth Karunarathna</h3>
+                <br>
+                <h5>No.118, Nilawadana Road, Homagama, </h5>
+                <h5>Colombo 08</h5>
+                <h5>071 92 54 679</h5>
+              </div>
+              <br>
+              <div class="row">
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-danger btn-block">Decline</button>
+                </div>
+                <div class="col-lg-6">
+                  <button type="button" class="btn btn-success btn-block">Verify</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
   </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
   </script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
   </script>
 
 </body>
+
 </html>
