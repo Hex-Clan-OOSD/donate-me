f3b276595fd73b5ab92251b06c55f0f4d72e8885
Thilina Sandekelum <thilinas.19@cse.mrt.ac.lk>
2022-01-03 15:15:40 +0530

    File u[ploading added

--- app/views/requests/add.php
+++ app/views/requests/add.php
@@ -8,10 +8,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
         integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo URLROOT;?>./css/home_styles.css" />
-    <link rel="stylesheet" href="<?php echo URLROOT;?>./css/request_add_form_styles.css" />
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/home_styles.css" />
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/request_add_form_styles.css" />
     <title>Details</title>
     <?php require APPROOT . '/views/inc/favicon.php';?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -60,14 +61,56 @@
                             rows="10" name="description" placeholder="Description"></textarea>
                         <span class="invalid-feedback"><?php echo $data['description_err']?></span>
                     </div>
+                    <form method="get" action="">Upload Evidence
+                        <!-- <input type="hidden" name="sid" value="1" /> -->
+                        <input type="file" name="file1" id="file1" />
+                        <button type="button" id="browse" class="btn btn-primary">Upload</button>
+                    </form>
 
 
-                    <button class="btn btn-primary button-submit" type="submit">Submit</button>
+
+
+                    <button type="button" class="btn btn-primary" data-toggle="modal"
+                        data-target="#exampleModal">Submit</button>
             </div>
         </div>
 
+        <!-- Button trigger modal -->
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Submit Confirmation</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Do you want to submit?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button class="btn btn-primary button-submit" type="submit">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         </form>
+
+        <script>
+            $(document).ready(function () {
+                $('input[id^=file]').hide();
+                $('#browse').click(function () {
+                    $('input[id^=file]').click();
+                })
+            });
+        </script>
+
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
         </script>
