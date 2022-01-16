472ed076faee24defc72d35af916498abdfacd62
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-01-16 12:30:04 +0530

    Refactoring done

--- app/config/config.php
+++ app/config/config.php
@@ -13,7 +13,13 @@
 
     // Upload Images location
     // Change here according to the path
-    define('UPLOAD_IMAGE_PATH',"/Applications/XAMPP/xamppfiles/htdocs/project/public/upload-images/");
+    $windowsPath = "";
+    $macPath = "/Applications/XAMPP/xamppfiles/htdocs/project/public/upload-images/";
+    $macUploadRequestPath = "/Applications/XAMPP/xamppfiles/htdocs/project/public/upload-images/requests/";
+    $macUploadDonationPath = "/Applications/XAMPP/xamppfiles/htdocs/project/public/upload-images/donations/";
+    define('UPLOAD_IMAGE_PATH',$macPath);
+    define('UPLOAD_IMAGE_PATH_REQUESTS',$macUploadRequestPath);
+    define('UPLOAD_IMAGE_PATH_DONATIONS',$macUploadDonationPath);
 
     //Site name
     define('SITENAME','DonateME');
